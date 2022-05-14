eb01db12e9bcc29ea4565cc5fa1f3ac711ca9507
kp <qkwoo9348@gmail.com>
2022-05-14 18:30:17 +0900

    0514 coment 가져오기 성공

--- test.py
+++ test.py
@@ -3,6 +3,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+
+
 
 # selenium에서 사용할 웹 드라이버 절대 경로 정보
 chromedriver = r'C:\Users\kp\Desktop\recipe_view\chromedriver_win32\chromedriver.exe'
@@ -10,14 +14,20 @@ chromedriver = r'C:\Users\kp\Desktop\recipe_view\chromedriver_win32\chromedriver
 driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
 
-url = 'https://www.youtube.com/watch?v=Mfnwc1dc0MY'
+url = 'https://www.youtube.com/watch?v=e--h7ax_6pQ'
 
 driver.get(url)
 driver.implicitly_wait(5)
 
-coment=driver.find('span', {'dir':'auto','class': 'style-scope yt-formatted-string'}).text
-#coment= coment.get_attribute("text")
+#html = driver.page_source
+#soup = BeautifulSoup(html, 'html.parser')
 
-print(coment)
+
+coments=driver.find_elements(By.CSS_SELECTOR, 'span.style-scope.yt-formatted-string')
+
+#coment= coment.get_attribute("text") 
+
+for coment in coments:
+    print(coment.text)
 
 # #description > yt-formatted-string > span:nth-child(6)
