ee2696f3d03dbb62bfb1adbec07a0f22a64ba777
kp <qkwoo9348@gmail.com>
2022-05-15 19:50:50 +0900

    0515 csv전달

--- crawling.py
+++ crawling.py
@@ -4,6 +4,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.common.by import By
+import csv
 
 # selenium에서 사용할 웹 드라이버 절대 경로 정보
 chromedriver = r'C:\Users\kp\Desktop\recipe_view\chromedriver_win32\chromedriver.exe'
@@ -26,10 +28,41 @@ while True:
     
     before_h = after_h
 
+excel_url=[]    # csv에 저장할 url
+excel_coment=[] # csv에 저장할 coment
 
 #url 가져오기
 url_lists=driver.find_elements_by_css_selector("a.yt-simple-endpoint.style-scope.ytd-grid-video-renderer")
 
+i=0
+
 for url_list in url_lists:
     url=url_list.get_attribute("href")
-    print(url)
+    driver2= webdriver.Chrome(chromedriver)
+    driver2.get(url)
+    driver2.implicitly_wait(5)
+    coments=driver2.find_elements(By.CSS_SELECTOR, 'span.style-scope.yt-formatted-string')
+    driver2.implicitly_wait(5)
+
+    # coment 문자열 길이가 100이 넘는 문자열만 url과 함께 저장
+    for coment in coments:
+        if len(coment.text)> 100:
+            excel_url.append(url)
+            excel_coment.append(coment.text)
+            break
+        else:
+            driver2.close
+            continue
+
+    # test 10개
+    if i>10:
+        break
+    else:
+        i=i+1
+    
+
+with open("test.csv", 'w') as file:
+    writer = csv.writer(file, delimiter=' ')
+    for j in range(len(excel_url)):
+        writer.writerow(excel_url[j],excel_coment[j])
+        
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
 # test용 파일
 # test 후 병합
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
-
+import csv
 
 
 # selenium에서 사용할 웹 드라이버 절대 경로 정보
@@ -14,14 +11,30 @@ chromedriver = r'C:\Users\kp\Desktop\recipe_view\chromedriver_win32\chromedriver
 driver = webdriver.Chrome(chromedriver)
 # driver로 특정 페이지를 크롤링한다.
 
-url = 'https://www.youtube.com/watch?v=e--h7ax_6pQ'
+url = 'https://www.youtube.com/watch?v=-bewV9LQ_JU'
 
 driver.get(url)
 driver.implicitly_wait(5)
 
 
 coments=driver.find_elements(By.CSS_SELECTOR, 'span.style-scope.yt-formatted-string')
+i=0
 
+excel_url=[]
+excel_coment=[]
 
 for coment in coments:
-    print(coment.text)
+    print(i)
+    if len(coment.text)> 100:
+        excel_url.append(url)
+        excel_coment.append(coment.text)
+    else:
+        continue
+    i=i+1
+
+f = open('write.csv','w', newline='')
+wr = csv.writer(f)
+
+wr.writerow([excel_url, excel_coment])
+
+f.close()
