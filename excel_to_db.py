02978f1ecb04226ee6222e77830cf80b0d464f0c
kp <qkwoo9348@gmail.com>
2022-05-18 19:13:36 +0900

    0518 데이터  가공 완료

--- excel_to_db.py
+++ excel_to_db.py
@@ -20,6 +20,7 @@ toDB_Title=[]
 toDB_Ingredients=[]
 toDB_Directions=[]
 
+'''
 for data in toDB_data:
     open = data.find('[')
     close = data.find(']')
@@ -50,9 +51,56 @@ for data in toDB_data:
         toDB_Directions.append(data[:open])
     else:      
         toDB_Directions.append(data)
+'''
+
+for data in toDB_data:
+    open = data.find('[')
+    close = data.find(']')
+    if open <0:
+        continue
+
+    find_title=data[open+1:close]
+    
+    if find_title.find('재료')<0:
+        continue
+    
+    open = data.find('=')
+    
+    data=data[open:].strip('=')
+
+    if open<0:
+        continue
+    
+    open = data.find('[')
+
+    if len(data[:open].strip('\n'))<1:
+        continue
+    
+    toDB_Title.append(data[:open].strip('\n'))
+    close=data.find(']')
+        
+    data=data[close+1:]
+    open=data.find('[')
+    close = data.find(']')
+    toDB_Ingredients.append(data[:open])
+    
+    data=data[open:]
+    close = data.find(']')
+    
+    data=data[close+1:]
+    
+    if data.find('자막')>0:
+        open = data.find('[')
+        toDB_Directions.append(data[:open])
+    else:      
+        toDB_Directions.append(data)
 
     
     
-print(toDB_Title)
+    
+    
+    
+print(toDB_Directions[6])
+    
 
 
