5a9c7d867016f0e73246411c52d763a1c5961b0b
kp <qkwoo9348@gmail.com>
2022-05-20 17:18:42 +0900

    0520 DB저장 완료

--- excel_to_db.py
+++ excel_to_db.py
@@ -1,5 +1,12 @@
-import openpyxl as xl
+# 엑셀 파일 가져오기
+# 데이터 가공
+# 이상데이터 처리
+# db 연동 및 데이터 전송
+# db admin/kpadmin12      db: test
 
+import openpyxl as xl
+import pymysql
+#엑셀 파일 위치
 dir = r"C:\Users\kp\Desktop\recipe_view\test.xlsx"
 
 # 엑셀 불러오기
@@ -11,17 +18,21 @@ excel_ws = excel['테스트']
 toDB_url=[]
 toDB_data=[]
 
+# 엑셀 value 리스트에 저장
 for i in range(2,excel_ws.max_row):
     toDB_url.append(excel_ws[f'A{i}'].value)
     toDB_data.append(excel_ws[f'B{i}'].value)
 excel.close()
 
+toDB_url2=[]
 toDB_Title=[]
 toDB_Ingredients=[]
 toDB_Directions=[]
 
-'''
+# 데이터 가공 / 이상값 넘기기
 for data in toDB_data:
+    i=toDB_data.index(data)
+    
     open = data.find('[')
     close = data.find(']')
     if open <0:
@@ -51,9 +62,13 @@ for data in toDB_data:
         toDB_Directions.append(data[:open])
     else:      
         toDB_Directions.append(data)
-'''
+    
+    toDB_url2.append(toDB_url[i])
 
+# 데이터 가공 이상값 처리
 for data in toDB_data:
+    i=toDB_data.index(data)
+
     open = data.find('[')
     close = data.find(']')
     if open <0:
@@ -63,7 +78,7 @@ for data in toDB_data:
     
     if find_title.find('재료')<0:
         continue
-    
+
     open = data.find('=')
     
     data=data[open:].strip('=')
@@ -95,12 +110,20 @@ for data in toDB_data:
     else:      
         toDB_Directions.append(data)
 
-    
-    
-    
-    
-    
-print(toDB_Directions[6])
-    
+    toDB_url2.append(toDB_url[i])
 
 
+
+# db에 저장
+conn = pymysql.connect(host='database-2.clufoxjoavjk.ap-northeast-2.rds.amazonaws.com', user='admin', password='kpadmin12', db='test', charset='utf8')
+
+sql = "INSERT INTO recipe (url, Title, Ingredients, Directions) VALUES (%s, %s,%s, %s)"
+
+
+with conn:
+    with conn.cursor() as cur:
+        for i in range(0,len(toDB_url2)):
+            cur.execute(sql, (toDB_url2[i], toDB_Title[i], toDB_Ingredients[i], toDB_Directions[i]))
+        conn.commit()
+        
+conn.close()
--- test.py
+++ test.py
@@ -1,28 +1,15 @@
-import openpyxl
+import pymysql
+import time
+conn = pymysql.connect(host='database-2.clufoxjoavjk.ap-northeast-2.rds.amazonaws.com', user='admin', password='kpadmin12', db='test', charset='utf8')
 
-dir = r"C:\Users\kp\Desktop\recipe_view\test.xlsx"
+sql = "SELECT * FROM recipe "
 
-# 경로에 있는 excel파일을 불러온다.
-excel = openpyxl.load_workbook(dir)
-
-# create_sheet() 메소드로 새로운 시트를 생성한다.
-excel.create_sheet('chocolate')
-excel.create_sheet('bread')
-
-excel.save(dir)
-excel = openpyxl.load_workbook(dir)
-
-# bread 시트를 선택
-excel_ws = excel['bread']
-
-# A1 셀을 직접 지정해 입력
-excel_ws['A1'] = 'HONEY'
-
-# 셀을 행과 열로 지정해 입력
-excel_ws.cell(row = 2, column = 1).value = 'Cheese'
-
-# 입력되어있는 바로 밑 행의 처음부터 입력된다.
-for x in range(10):
-    excel_ws.append(['I', 'like', 'bread'])
-
-excel.save(dir)
+with conn:
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        result = cur.fetchall()
+        for data in result:
+            print(data)
+            time.sleep(3)
+            print('\n')
+        
