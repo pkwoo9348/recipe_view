4e3d0746d2f0ffa2d2fe7ec224b34caa4610af6f
kp <qkwoo9348@gmail.com>
2022-05-21 18:00:45 +0900

    0521 스프링부트설치

---
+++ demo/src/main/java/com/example/demo/Apicontroller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RestController
+public class Apicontroller {
+    
+    @RequestMapping(value ="/api/test",method =RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String getApiTest(){
+        return "{\"result\": \"ok\"}";
+    }
+}
